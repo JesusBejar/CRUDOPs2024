99b772633fd10a46d3ee98931eefa2b53ca58cd4
Jesus Bejar <israelbejarii@gmail.com>
2024-01-27 15:08:28 -0700

    added .get & .delete routes

--- routes/listings.js
+++ routes/listings.js
@@ -4,9 +4,13 @@ const listingsController = require('../controllers/listings');
 // const validator = require('../middleware/validate');
 
 // these match with functions in ./controllers/listings.js
+// GET ALL
+router.get('/', contactsController.getAll);
 // CREATE
 router.post('/', listingsController.createListing)
 // UPDATE
 router.put('/:id', listingsController.updateListing)
+// DELETE
+router.delete('/', listingsController.updateListing)
 
 module.exports = router;
