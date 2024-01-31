37e531334445ccbf2b39d7b080cd841854866b2c
Jesus Bejar <israelbejarii@gmail.com>
2024-01-30 20:40:55 -0700

    wrote isAuthenticated calls & login, logout routes

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,17 @@ const router = require('express').Router();
 router.get('/', (req, res) => {res.send('hola mundo')});
 router.use ('/listings', require('./listings'))
 
+// log in
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+// log out
+router.get('/logout', function(req, res, next){
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
--- routes/listings.js
+++ routes/listings.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router  = express.Router();
 const listingsController = require('../controllers/listings');
+// authentication code found in authenticate.js & index.js
+const { isAuthenticated } = require('authenticate.js')
 // const validator = require('../middleware/validate');
 
 // these match with functions in ./controllers/listings.js
 // GET ALL
 router.get('/', listingsController.getAllListings);
 // CREATE
-router.post('/', listingsController.createListing)
+router.post('/', isAuthenticated, listingsController.createListing)
 // UPDATE
-router.put('/:id', listingsController.updateListing)
+router.put('/:id', isAuthenticated, listingsController.updateListing)
 // DELETE
-router.delete('/', listingsController.deleteListing)
+router.delete('/', isAuthenticated, listingsController.deleteListing)
 
 module.exports = router;
